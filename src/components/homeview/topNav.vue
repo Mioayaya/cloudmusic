97f794e90b68b43479eb4c38bdd75616ece25a1d
raften <1530020617@qq.com>
2022-04-17 23:08:47 +0800

    顶部logo

--- src/components/homeview/topNav.vue
+++ src/components/homeview/topNav.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="topnav">
-        导航栏
+        <div class="left">
+            <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-wangyiyun"></use>
+            </svg>
+            <span class="text">网易云音乐</span>
+        </div>
+
     </div>
 </template>
 
@@ -9,5 +15,29 @@
 </script>
 
 <style lang="less" scoped>
-
+.topnav {
+    width: 100%;
+    height: 65px;
+    background-color: black;
+    display: flex;
+    position: relative;
+    align-items: center;
+    padding-left: 20px;
+    .left {
+        position: relative;
+        color: white;
+        justify-content: left;
+        display: flex;
+        .icon {
+            width: 25px;
+            height: 25px;
+            margin-right: 10px;
+        }
+        .text {
+            font-size: 18px;
+            line-height: 30px;
+            margin-top: -3px;
+        }
+    }
+}
 </style>
