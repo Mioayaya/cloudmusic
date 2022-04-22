0ba8533a7fa56fc9d07a543157053d64ea9ad8af
raften <1530020617@qq.com>
2022-04-22 23:02:07 +0800

    页内滚动

---
+++ src/components/homeview/homebody/perRecomdCont.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="person-content">
+        <el-scrollbar height="74.7vh">
+            <SwiperNav/>
+            <RecomdList/>
+        </el-scrollbar>
+            
+    </div>
+</template>
+
+<script setup>
+import SwiperNav from './personrecommend/swiperNav.vue';
+import RecomdList from './personrecommend/recomdList.vue';
+
+</script>
+
+<style lang="less" scoped>
+.person-content {
+    padding: 3vh 0 0 7vw;
+    width: 100%;
+    height: auto;
+
+}
+</style>
