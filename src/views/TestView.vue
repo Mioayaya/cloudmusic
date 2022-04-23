9fd5794c67ea462549aab2748449a3b8e8dba460
raften <1530020617@qq.com>
2022-04-23 23:39:25 +0800

    测试view

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="test">测试</div>
+    <div class="test">
+        <TopNav/>
+        <div class="content">
+            <ListNav/>
+            <span>测试view</span>
+        </div>
+        <PlayBottom/>
+    </div>
 </template>
 
 <script setup>
+import TopNav from '../components/homeview/topNav.vue';
+import ListNav from '../components/homeview/listNav.vue';
+import PlayBottom from '../components/homeview/playBottom.vue';
 
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.content {
+    display: flex;
+    flex-direction: row;
+}
 </style>
