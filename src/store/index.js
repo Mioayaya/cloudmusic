715b03eaf0e667227ea802056a08838486ed934d
raften <1530020617@qq.com>
2022-04-28 20:29:16 +0800

    播放相关的全局变量

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,12 @@ export default createStore({
     state: {
         // 数据存放
         listActive: 1,
+        // 播放列表
+        playlist: [],
+        // 当前播放的音乐
+        playCurrentIndex: 0,
+        // 当前播放是第几首
+        playId: 0,
     },
     getters: {
     },
@@ -11,11 +17,30 @@ export default createStore({
         listActive2(state, active) {
             state.listActive = active;
         },
+        // 修改播放列表
+        setPlaylist2(state,value) {
+            state.playlist = value;
+        },
+        setPlayIndex2(state,index) {
+            state.playCurrentIndex = index;
+        },
+        setPlayId2(state,id) {
+            state.playId = id;
+        }
     },
     actions: {
         listActive(context, active) {
             context.commit('listActive2', active);
         },
+        setPlaylist(context, value) {
+            context.commit('setPlaylist2', value);
+        },
+        setPlayIndex(context, index) {
+            context.commit('setPlayIndex2', index);
+        },
+        setPlayId(context, id) {
+            context.commit('setPlayId2', id);
+        }
     },
     modules: {
     }
